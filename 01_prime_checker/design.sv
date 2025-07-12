311295690ddc64bf7c5e2a1ae279bf168e9a15df
Yusif Bahlool <148661949+YBahlool@users.noreply.github.com>
2025-07-12 20:06:58 +0300

    Create design.sv

---
+++ 01_prime_checker/design.sv
@@ -0,0 +1,21 @@
+// Code your design here
+module challenge1 (
+  input C,
+  input B,
+  input A, 
+  output P
+
+	);
+  
+  wire notToAnd;
+  wire andToOr;
+  wire andToOrBottom;
+  
+  
+  not Not_Gate(notToAnd, C);
+  and And_Gate(andToOr, notToAnd, B);
+  and b_and_gate(andToOrBottom, A, C);
+  or final_or(P, andToOrBottom, andToOr);
+  
+endmodule
+  
