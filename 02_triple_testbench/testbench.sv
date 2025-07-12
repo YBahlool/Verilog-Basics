236d3382a143cb8ef3015e376057fac7a2d92e2d
Yusif Bahlool <148661949+YBahlool@users.noreply.github.com>
2025-07-12 20:17:17 +0300

    Update testbench.sv

--- 02_triple_testbench/testbench.sv
+++ 02_triple_testbench/testbench.sv
@@ -1,1 +1,22 @@
-
+module Triple_TB();
+  reg [3:0] a;
+  wire [5:0] result;
+  
+  
+  triple FL(a, result);
+  
+  initial begin
+    $monitor ("The triple of %d is %d", a, result);
+    a = 0; #10;
+    a = 1; #10;
+    a = 3; #10;
+    a = 5; #10;
+    a = 10; #10;
+    a = 15; #10;
+    
+    $finish;
+  end
+  
+  
+  
+endmodule
