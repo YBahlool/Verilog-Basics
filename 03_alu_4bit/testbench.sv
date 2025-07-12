acf4e5a809f9f5d074f76bec394358b4b6b3049a
Yusif Bahlool <148661949+YBahlool@users.noreply.github.com>
2025-07-12 20:23:15 +0300

    Update testbench.sv

--- 03_alu_4bit/testbench.sv
+++ 03_alu_4bit/testbench.sv
@@ -1,1 +1,21 @@
-
+module ALU_Test();
+  reg [3:0] firstNum;
+  reg [3:0] secNum;
+  reg [1:0] decideOp;
+  wire [7:0] result;
+  
+  ALU FL(firstNum, secNum, decideOp, result);
+  
+  
+  initial begin
+    $monitor ("The result of the operator is %d", result);
+    
+    firstNum = 3; secNum = 2; decideOp = 0; #10;
+    
+    
+    
+    $finish;
+  end
+  
+  
+endmodule
